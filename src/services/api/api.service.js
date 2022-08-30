2a68f9dde1d740355f283a55426c285a25b0119d
ignatiqq <89993147+ignatiqq@users.noreply.github.com>
2022-08-30 11:22:02 +0300

    Update api.service.js

--- src/services/api/api.service.js
+++ src/services/api/api.service.js
@@ -25,20 +25,24 @@ export const getIcon = icon => {
 };
 
 export const getWeather = async city => {
-  const token = process.env.TOKEN ?? (await getKeyValue(TOKENDICTIONARY.token));
+  try {
+    const token = process.env.TOKEN ?? (await getKeyValue(TOKENDICTIONARY.token));
 
-  if (!token) {
-    throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
+    if (!token) {
+      throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
+    }
+
+    const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        q: city,
+        appid: token,
+        lang: 'ru',
+        units: 'metric',
+      },
+    });
+
+    return data;
+  } catch(e) {
+    // error catching
   }
-
-  const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
-    params: {
-      q: city,
-      appid: token,
-      lang: 'ru',
-      units: 'metric',
-    },
-  });
-
-  return data;
 };
